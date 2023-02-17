a509c52fdeebfb411924267f164ffb1b8828572d
Kridtima <125581879+kridtima13@users.noreply.github.com>
2023-02-17 12:16:35 +0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./main.scss";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById("root"));
 
-serviceWorker.unregister();
+// const name = 'John Doe';
+// const myfirstelement = <h1>Hello {name} :)</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
